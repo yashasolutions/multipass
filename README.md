ceee52333cd8747579fa49d3d31733338b8554c9
Yasha <3-yasha@users.noreply.gitlab.bbdev.team>
2024-02-03 20:32:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# Multipost
+
+Currently, it is not doing much. Just a thin wrapper around cli tools to post on mastodon and twitter/X.
+
+
+## Dependancy
+- tkinter
+- x
+- toot 
+
+## Roadmap
+
+The real long term plan is to have a normal tool to work in current daily absurdity that social media platform have become. 
+
+- [ ] rewrite integration in python for twitter and mastodon
+- [ ] linkedin
+- [ ] instagram
+- [ ] Facebook
+- [ ] bluesky 
+- [ ] Threads.net
+- [ ] Telegram channels
+- [ ] sqlite local cache of tweet to implement draft mechanism
+- [ ] schedule
+- [ ] thread supports (auto thread)
+- [ ] binary release
---
+++ app.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+import TKinterModernThemes as TKMT
+from TKinterModernThemes.WidgetFrame import Widget
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import Text
+import subprocess
+
+
+
+def quitCMD():
+    exit()
+
+class App(TKMT.ThemedTKinterFrame):
+    def __init__(self):
+        super().__init__("Posting", "park", "light")
+        self.image_path = ""
+
+        self.textinputvar = tk.StringVar()
+        self.textFrame = self.addLabelFrame("Your Post")
+        self.textFrame.t = Text(self.textFrame.master,
+                      highlightthickness = 0,
+                      borderwidth=0,
+                      blockcursor=True,
+                      font=("Courier", 12),
+                      wrap="word",
+                      width=40, height=10)
+        self.textFrame.t.grid(row=0, column=0, padx=10, pady=10)
+        self.textFrame.t.bind("<KeyRelease>", self.textupdate)
+        self.Button("Upload Image", self.upload_image)
+        self.Button("Post", self.post)
+        self.Button("Quit", quitCMD)
+        self.run()
+
+    def textupdate(self, event):
+        print("Current text status:", self.textFrame.t.get("1.0", "end-1c"))
+        self.textinputvar.set(self.textFrame.t.get("1.0", "end-1c"))
+
+    def printCMD(self):
+        print("Button clicked!")
+        print(self.textinputvar.get())
+        print(self.image_path)
+
+    def upload_image(self):
+        self.image_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png")],
+                                                     title="Select an image",
+                                                     initialdir="~/img/")
+    def post(self):
+        message = self.textinputvar.get()
+        masto_cli = ""
+        twitter_cli = ""
+
+        if not message:
+            print("No message to post")
+            return
+
+        if self.image_path:
+            masto_cli = f"toot post -m {self.image_path} \"{message}\""
+            twitter_cli = "echo \"No support for images in api\""
+        else:
+            masto_cli = f"toot post \"{message}\""
+            twitter_cli = f"~/.local/bin/x -t \"{message}\""
+
+        subprocess.run(masto_cli, shell=True)
+        subprocess.run(twitter_cli, shell=True)
+
+        exit()
+
+if __name__ == "__main__":
+    App()
